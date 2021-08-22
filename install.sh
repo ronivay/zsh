7a70f3117498a7165028a0ae5b5db161ba256c4b
ronivay <roni@vayrynen.info>
2021-08-22 20:00:21 +0300

    finetune install

--- install.sh
+++ install.sh
@@ -8,24 +8,19 @@ ZDIR=$HOME/.config/zsh
 [ ! $(command -v git) ] && { echo "install git first" ; exit 1; }
 
 if ! [ -d $ZDIR ]; then
-	echo "Cloning https://github.com/ronivay/zsh to $ZDIR"
-	echo
+	echo "+++ Cloning https://github.com/ronivay/zsh to $ZDIR"
 	git clone "https://github.com/ronivay/zsh" $ZDIR
-	echo
-	echo "Installing default plugins and theme"
+	echo "+++ Installing default plugins and theme"
 	zsh $ZDIR/prep.sh
 fi
 
-echo
-echo "Adding config to ~/.zshrc"
+echo "+++ Adding config to ~/.zshrc"
 cat <<'EOF' >> $HOME/.zshrc
 export ZDIR=$HOME/.config/zsh
 source $ZDIR/zshrc
 EOF
 
-echo
-echo "Changing default shell to zsh"
+echo "+++ Changing default shell to zsh"
 chsh -s $(command -v zsh)
 
-echo
-echo "Done. Restart shell to load"
+echo "+++ Done. Restart shell to load"
