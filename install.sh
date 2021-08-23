51c4558f2361222c2f0365326dfb1188be3ec7b9
ronivay <roni@vayrynen.info>
2021-08-23 08:46:16 +0300

    finetune plugin install and install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 set -ae
 
@@ -13,7 +13,30 @@ if ! [ -d $ZDIR ]; then
 	git clone "https://github.com/ronivay/zsh" $ZDIR
 	echo "+++ Installing default plugins and theme"
 	echo
-	zsh $ZDIR/prep.sh
+	source "$ZDIR/zsh-config"
+
+	for PLUGIN in ${zsh_plugins[@]}; do
+	if ! [ -d $ZDIR/plugins/${PLUGIN##*/} ]; then
+		mkdir -p "$ZDIR/plugins"
+		git clone "https://github.com/$PLUGIN.git" "$ZDIR/plugins/${PLUGIN##*/}"
+	fi
+	done
+
+	if [ ${#zsh_ohmyzsh_plugins[@]} -gt 0 ]; then
+		if ! [ -d $ZDIR/plugins/ohmyzsh ]; then
+			mkdir -p "$ZDIR/plugins"
+			git clone "https://github.com/ohmyzsh/ohmyzsh.git" "$ZDIR/plugins/ohmyzsh"
+		fi
+	fi
+
+	if [ -n $zsh_theme ]; then
+		if ! [ -d "$ZDIR/themes/${zsh_theme##*/}" ]; then
+			git clone "https://github.com/$zsh_theme.git" "$ZDIR/themes/${zsh_theme##*/}"
+		fi
+	fi
+else
+	echo "$ZDIR already exists, not continuing"
+	exit 1
 fi
 
 echo
